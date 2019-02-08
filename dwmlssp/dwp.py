759fe102b9ee90be558a9f7a4bdfa0d18a3593ef
nosbor23 <46794771+nosbor23@users.noreply.github.com>
2019-02-08 11:23:46 -0200

    Update dwp.py

--- dwmlssp/dwp.py
+++ dwmlssp/dwp.py
@@ -765,8 +765,6 @@ if __name__=="__main__":
             for te in T:
                 for v in V:
 		    #alterar caminho aqui
-                    H = pickle.load('/home/servidor-lasos/Thiago/Robson/DW/dwmlssp/inst/py_inst_p'+str(pr)+'f'+str(fo)+'t'+str(te)+'_'+str(v)+'.dat', 'r')
-	            pickle.dump(H, open('/home/servidor-lasos/Thiago/Robson/DW/dwmlssp/inst/py_inst_p'+str(pr)+'f'+str(fo)+'t'+str(te)+'_'+str(v)+'_bin.dat', 'wb'))
                     I = loadPickledInst('/home/servidor-lasos/Thiago/Robson/DW/dwmlssp/inst/py_inst_p'+str(pr)+'f'+str(fo)+'t'+str(te)+'_'+str(v)+'_bin.dat')
                     P = I.P
                     F = I.F
