045fbe01de8722d75fdf1c17783362d183add42d
SERVIDOR-LASOS <thiagoep@gmail.com>
2019-02-11 10:07:04 -0500

    teste

--- dwmlssp/rodar2.sh
+++ dwmlssp/rodar2.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
 echo "Inicio dos testes"
 
-python dwp.py
+python dwp1.py
+
+echo "Testes finalizados, compactando..."
+cp log0802.txt ~/Thiago/Robson/DW/dwmlssp/result/
+python zipador.py
+echo "compactado, enviando e-mail..."
+python semail.py
+echo "Finalizado"
+echo
+
+echo "Inicio dos testes"
+
+python dwp2.py
+
+echo "Testes finalizados, compactando..."
+cp log0802.txt ~/Thiago/Robson/DW/dwmlssp/result/
+python zipador.py
+echo "compactado, enviando e-mail..."
+python semail.py
+echo "Finalizado"
+echo
+
+echo "Inicio dos testes"
+
+python dwp3.py
+
+echo "Testes finalizados, compactando..."
+cp log0802.txt ~/Thiago/Robson/DW/dwmlssp/result/
+python zipador.py
+echo "compactado, enviando e-mail..."
+python semail.py
+echo "Finalizado"
+echo
+
+echo "Inicio dos testes"
+
+python dwp4.py
 
 echo "Testes finalizados, compactando..."
 cp log0802.txt ~/Thiago/Robson/DW/dwmlssp/result/
